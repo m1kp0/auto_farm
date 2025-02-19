f9864b88e54de131914cd83ea1aa8a6fc5d27ddc
m1kpe0 <yarikasik@inbox.ru>
2025-02-20 00:44:09 +0300

    Update bear_alpha.lua

--- bear_alpha.lua
+++ bear_alpha.lua
@@ -11,8 +11,8 @@ w:Toggle("auto farm", false, function(e)
     farm = e
     repeat
         if farm then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(400, 28, -27)
-            game.Players.LocalPlayer.Character.Humanoid.WalkToPoint = Vector3.new(408, 28, -27)
+            hrp.CFrame = CFrame.new(400, 28, -27)
+            h.WalkToPoint = Vector3.new(408, 28, -27)
             wait(8.5)
         end
     until farm == false
